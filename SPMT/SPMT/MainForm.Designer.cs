2d792a3b4b6f8a48fc17ee119d5f68b3e9d1bc45
Andrewsimsd <andrew.sims.d@gmail.com>
2022-07-18 19:06:03 -0400

    basic GUI setup

--- SPMT/SPMT/MainForm.Designer.cs
+++ SPMT/SPMT/MainForm.Designer.cs
@@ -30,12 +30,18 @@ namespace SPMT
         private void InitializeComponent()
         {
             this.tlpNavButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSecurityLookup = new System.Windows.Forms.Button();
+            this.pnlCurrentForm = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tlpNavButtons.SuspendLayout();
             this.SuspendLayout();
             // 
             // tlpNavButtons
             // 
             this.tlpNavButtons.ColumnCount = 1;
             this.tlpNavButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpNavButtons.Controls.Add(this.btnSecurityLookup, 0, 1);
+            this.tlpNavButtons.Controls.Add(this.button1, 0, 2);
             this.tlpNavButtons.Dock = System.Windows.Forms.DockStyle.Left;
             this.tlpNavButtons.Location = new System.Drawing.Point(0, 0);
             this.tlpNavButtons.Name = "tlpNavButtons";
@@ -46,18 +52,53 @@ namespace SPMT
             this.tlpNavButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tlpNavButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlpNavButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tlpNavButtons.Size = new System.Drawing.Size(200, 502);
+            this.tlpNavButtons.Size = new System.Drawing.Size(200, 460);
             this.tlpNavButtons.TabIndex = 0;
+            // 
+            // btnSecurityLookup
+            // 
+            this.btnSecurityLookup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSecurityLookup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSecurityLookup.ForeColor = System.Drawing.Color.White;
+            this.btnSecurityLookup.Location = new System.Drawing.Point(3, 123);
+            this.btnSecurityLookup.Name = "btnSecurityLookup";
+            this.btnSecurityLookup.Size = new System.Drawing.Size(194, 34);
+            this.btnSecurityLookup.TabIndex = 0;
+            this.btnSecurityLookup.Text = "Secutiry Lookup";
+            this.btnSecurityLookup.UseVisualStyleBackColor = true;
+            this.btnSecurityLookup.Click += new System.EventHandler(this.btnSecurityLookup_Click);
+            // 
+            // pnlCurrentForm
+            // 
+            this.pnlCurrentForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCurrentForm.Location = new System.Drawing.Point(200, 0);
+            this.pnlCurrentForm.Name = "pnlCurrentForm";
+            this.pnlCurrentForm.Size = new System.Drawing.Size(670, 460);
+            this.pnlCurrentForm.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(3, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Another Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.ClientSize = new System.Drawing.Size(753, 502);
+            this.ClientSize = new System.Drawing.Size(870, 460);
+            this.Controls.Add(this.pnlCurrentForm);
             this.Controls.Add(this.tlpNavButtons);
             this.Name = "MainForm";
             this.Text = "SMPT";
+            this.tlpNavButtons.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -65,6 +106,9 @@ namespace SPMT
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tlpNavButtons;
+        private System.Windows.Forms.Button btnSecurityLookup;
+        private System.Windows.Forms.Panel pnlCurrentForm;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SPMT/SPMT/MainForm.cs
+++ SPMT/SPMT/MainForm.cs
@@ -12,9 +12,36 @@ namespace SPMT
 {
     public partial class MainForm : Form
     {
+        private View.SecurityLookupForm securityLookupForm = new View.SecurityLookupForm();
+        private View.AnotherForm anotherForm = new View.AnotherForm();
+
         public MainForm()
         {
             InitializeComponent();
+            securityLookupForm.TopLevel = false;
+            securityLookupForm.Dock = DockStyle.Fill;
+            this.pnlCurrentForm.Controls.Clear();
+            this.pnlCurrentForm.Controls.Add(securityLookupForm);
+            securityLookupForm.Show();
+        }
+
+        private void btnSecurityLookup_Click(object sender, EventArgs e)
+        {
+            securityLookupForm.TopLevel = false;
+            securityLookupForm.Dock = DockStyle.Fill;
+            this.pnlCurrentForm.Controls.Clear();
+            this.pnlCurrentForm.Controls.Add(securityLookupForm);
+            securityLookupForm.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            anotherForm.TopLevel = false;
+            anotherForm.Dock = DockStyle.Fill;
+            this.pnlCurrentForm.Controls.Clear();
+            this.pnlCurrentForm.Controls.Add(anotherForm);
+            anotherForm.Show();
         }
     }
 }
